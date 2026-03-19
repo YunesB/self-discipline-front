db68abfeb5adb0530f670920046508bf325b4695
Yunes B <yunes.bakhush@gmail.com>
2026-03-19 11:17:53 +0500

    feat: habits table added

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,55 +1,144 @@
 import { FC } from "react";
 
+import dayjs from "dayjs";
 import { useUnit } from "effector-react";
+import { ClassNameValue } from "tailwind-merge";
 
 import { MOCK_HABITS } from "./lib";
 import { $$dashboardModel } from "./model";
 
 import { HOLIDAYS } from "@/shared/lib/constants/calendar";
 import { cn } from "@/shared/lib/utils";
+import { Checkbox } from "@/shared/ui/shadcn";
+
+const COMMON_CLASSES = "p-4 border border-collapse";
+const HEADER_CLASSES = "text-sm";
+const DAY_WIDTH = "min-w-[60px]";
+const GOAL_WIDTH = "min-w-[110px]";
+
+const getPercentageColor = (percentage: number): ClassNameValue => {
+  if (percentage >= 100) return "text-green-500";
+  if (percentage > 60) return "text-cyan-600";
+  if (percentage > 30) return "text-yellow-500";
+  if (percentage > 0) return "text-orange-500";
+  return "text-red-500";
+};
 
 export const DashboardPage: FC = () => {
+  const today = dayjs().date();
   const [calendarByWeeks] = useUnit([$$dashboardModel.$calendarByWeeks]);
 
   return (
-    <div className="flex items-start gap-x-12">
-      <ul className="pt-8">
-        {MOCK_HABITS.map(({ name }, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
+    <div className="p-4 rounded-xl bg-white max-w-full overflow-auto">
+      <table className="w-full border-collapse border">
+        <thead>
+          <tr>
+            <th rowSpan={2} className={cn(COMMON_CLASSES, HEADER_CLASSES)}>
+              Habit
+            </th>
+            <th
+              rowSpan={2}
+              className={cn(COMMON_CLASSES, GOAL_WIDTH, HEADER_CLASSES)}
+            >
+              Goal
+            </th>
+            <th
+              rowSpan={2}
+              className={cn(COMMON_CLASSES, GOAL_WIDTH, HEADER_CLASSES)}
+            >
+              Progress
+            </th>
+            {calendarByWeeks.map((_, index) => (
+              <th
+                key={index}
+                colSpan={7}
+                className={cn(
+                  COMMON_CLASSES,
+                  "text-sm font-normal",
+                  index % 2 === 0 ? "bg-gray-50 shadow-inner" : "bg-white",
+                )}
+              >
+                Week {index + 1}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {calendarByWeeks.map((week) => (
+              <>
+                {week.map(({ date, day }, dayIndex) => (
+                  <th
+                    key={dayIndex}
+                    className={cn(
+                      DAY_WIDTH,
+                      COMMON_CLASSES,
+                      HOLIDAYS.includes(day) && "text-green-500",
+                      today === date && "bg-blue-50 text-blue-400",
+                    )}
+                  >
+                    <p>{date}</p>
+                    <p className="text-[10px] font-normal">{day.slice(0, 3)}</p>
+                  </th>
+                ))}
+              </>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {MOCK_HABITS.map(
+            ({ name, monthlyGoal, days, checkedDates }, index) => {
+              const percentage = Math.round(
+                (checkedDates.length / monthlyGoal) * 100,
+              );
 
-      <ul className="flex items-center">
-        {calendarByWeeks.map((week, index) => (
-          <li
-            key={index}
-            className={cn(
-              "px-4 py-2",
-              index % 2 === 0 ? "bg-gray-100" : "bg-white",
-              index === 0 && "rounded-l-xl",
-              index === calendarByWeeks.length - 1 && "rounded-r-xl",
-            )}
-          >
-            <p className="text-center whitespace-nowrap mb-2">
-              Week {index + 1}
-            </p>
-            <ul className="flex items-center gap-4">
-              {week.map(({ date, day }, dayIndex) => (
-                <li
-                  key={dayIndex}
-                  className={cn(
-                    "text-center",
-                    HOLIDAYS.includes(day) && "text-green-500",
+              return (
+                <tr key={index}>
+                  <td
+                    className={cn(COMMON_CLASSES, "whitespace-nowrap text-sm")}
+                  >
+                    {name}
+                  </td>
+                  <td className={cn(COMMON_CLASSES, "text-right text-sm")}>
+                    {monthlyGoal}
+                  </td>
+                  <td
+                    className={cn(
+                      COMMON_CLASSES,
+                      "text-right text-sm",
+                      getPercentageColor(percentage),
+                    )}
+                  >
+                    {checkedDates.length} ({percentage}%)
+                  </td>
+
+                  {calendarByWeeks.map((week, weekIndex) =>
+                    week.map(({ day, date }, dayIndex) => {
+                      const hasCheckbox = days.includes(day);
+
+                      return (
+                        <td
+                          key={`${weekIndex}-${dayIndex}`}
+                          className={cn(
+                            !hasCheckbox && "bg-gray-50 shadow-inner",
+                            date === today && "bg-blue-50/70",
+                          )}
+                        >
+                          <div className="flex items-center justify-center">
+                            {hasCheckbox && (
+                              <Checkbox
+                                defaultChecked={checkedDates.includes(date)}
+                              />
+                            )}
+                          </div>
+                        </td>
+                      );
+                    }),
                   )}
-                >
-                  <p className="[10px]">{day.slice(0, 3)}</p>
-                  <p>{date}</p>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+                </tr>
+              );
+            },
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/pages/dashboard/lib.ts
+++ src/pages/dashboard/lib.ts
@@ -1,44 +1,69 @@
-export const MOCK_HABITS = [
+import { TDayOfWeek } from "@/shared/lib/constants/calendar";
+
+export type THabit = {
+  name: string;
+  color: string;
+  days: TDayOfWeek[];
+  monthlyGoal: number;
+  checkedDates: number[];
+};
+
+export const MOCK_HABITS: THabit[] = [
   {
     name: "Morning Routine",
     color: "#FF5733",
     days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
     monthlyGoal: 20,
+    checkedDates: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ],
   },
   {
     name: "Exercise",
     color: "#33FF57",
     days: ["Monday", "Wednesday", "Friday"],
     monthlyGoal: 15,
+    checkedDates: [1, 3, 5, 8, 10, 12, 15, 17, 19, 22, 24, 26, 29],
   },
   {
     name: "Reading",
     color: "#3357FF",
     days: ["Tuesday", "Thursday"],
     monthlyGoal: 10,
+    checkedDates: [2, 4, 9, 11, 16, 18, 23, 25, 30],
   },
   {
     name: "Meditation",
     color: "#FF33A1",
     days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
     monthlyGoal: 25,
+    checkedDates: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    ],
   },
   {
     name: "Healthy Eating",
     color: "#33FFF6",
     days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
     monthlyGoal: 30,
+    checkedDates: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ],
   },
   {
     name: "Sleep Schedule",
     color: "#F633FF",
     days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
     monthlyGoal: 28,
+    checkedDates: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    ],
   },
   {
     name: "Learning a New Skill",
     color: "#33FF9E",
     days: ["Monday", "Wednesday", "Friday"],
     monthlyGoal: 12,
+    checkedDates: [1, 3, 5, 8, 10, 12, 15, 17, 19, 22, 24],
   },
 ];
